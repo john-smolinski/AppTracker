de1cea38cf10def8e4f2ad6d18d961475dcb28a6
John Smolinski <john.smolinski@gmail.com>
2025-01-18 18:09:20 -0800

    moving pages to Pages folder structure

--- webui/app-tracker/src/App.js
+++ webui/app-tracker/src/App.js
@@ -1,10 +1,10 @@
 import "../src/styles/app.css";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Home from "./components/Home";
-import SearchApplications from "./components/SearchApplications";
-import AddApplication from "./components/AddApplication";
+import Home from "./components/Pages/Home/Home";
+import SearchApplications from "./components/Pages/Search/SearchApplications";
+import AddApplication from "./components/Pages/AddApplication/AddApplication";
 import NotFound from "./components/NotFound";
-import Statistics from "./components/Statistics";
+import Statistics from "./components/Pages/Statistics/Statistics";
 
 function App() {
   return (
--- webui/app-tracker/src/components/Pages/Home/Home.js
+++ webui/app-tracker/src/components/Pages/Home/Home.js
@@ -1,7 +1,7 @@
 import React from "react";
-import Menu from "./Menu/Menu";
-import Summary from "./Summary";
-import ApplicationCalendar from "./ApplicationCalendar";
+import Menu from "../../Menu/Menu";
+import Summary from "../../Summary";
+import ApplicationCalendar from "../../ApplicationCalendar";
 
 export default function Home() {
   return (
