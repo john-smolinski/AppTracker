915cc8baeb957bc11526e8d7528eb03f1ea8ed4c
John Smolinski <john.smolinski@gmail.com>
2025-01-24 08:54:02 -0800

    adding some basic stying around the page

--- webui/app-tracker/src/components/Pages/ApplicationsView/ApplicationsView.js
+++ webui/app-tracker/src/components/Pages/ApplicationsView/ApplicationsView.js
@@ -4,12 +4,14 @@ import { fetchSources } from "../../../redux/sourcesSlice";
 import { fetchJobTitles } from "../../../redux/jobTitlesSlice";
 import { fetchWorkEnvironments } from "../../../redux/workEnvironmentsSlice";
 import Menu from "../../Menu/Menu";
+import "../../../App.css";
+import "./ApplicationsView.css";
 
 export default function ApplicationsView() {
   const dispatch = useDispatch();
 
   useEffect(() => {
-    // fetch supporting data on start
+    // fetch supporting data on start - may consider moving this to the App.js
     dispatch(fetchSources());
     dispatch(fetchJobTitles());
     dispatch(fetchWorkEnvironments());
@@ -18,7 +20,13 @@ export default function ApplicationsView() {
   return (
     <div className="app">
       <Menu />
-      <h1>Search Placeholder</h1>
+      <h1>Applications Submitted</h1>
+      <div className="applications">
+        <div>filter and search bar</div>
+        <div>
+          <p>all applications listed here</p>
+        </div>
+      </div>
     </div>
   );
 }
