2b1dcb2cfa609171eea3a9f6d9823ec300bd20d4
John Smolinski <john.smolinski@gmail.com>
2025-01-24 15:28:20 -0800

    initial implementation of mui DataGrid

--- webui/app-tracker/src/components/Pages/ApplicationsView/ApplicationsView.js
+++ webui/app-tracker/src/components/Pages/ApplicationsView/ApplicationsView.js
@@ -1,96 +1,41 @@
-import React, { useEffect, useState } from "react";
-import { useDispatch, useSelector } from "react-redux";
-import { fetchSources } from "../../../redux/sourcesSlice";
-import { fetchJobTitles } from "../../../redux/jobTitlesSlice";
-import { fetchWorkEnvironments } from "../../../redux/workEnvironmentsSlice";
+import React from "react";
+import { useSelector } from "react-redux";
+import { DataGrid } from "@mui/x-data-grid";
 import Menu from "../../Menu/Menu";
 import "../../../App.css";
 import "./ApplicationsView.css";
 
 export default function ApplicationsView() {
-  const dispatch = useDispatch();
+  // application data from state
+  const apps = useSelector((state) => state.applications.apps);
 
-  // dispatch actions to get filter values
-  useEffect(() => {
-    // fetch supporting data on start - may consider moving this to the App.js
-    dispatch(fetchSources());
-    dispatch(fetchJobTitles());
-    dispatch(fetchWorkEnvironments());
-  }, [dispatch]);
+  // reformat the application data in a way that is easier to bind to the DataGrid
+  const formattedAppData = apps.map((item) => ({
+    id: item.id,
+    applicaitionDate: item.applicaitionDate,
+    source: item.source.name,
+    organization: item.organization.name,
+    jobTitle: item.jobTitle.name,
+    workEnvironment: item.workEnvironment.name,
+  }));
 
-  // filter values from state
-  const sources = useSelector((state) => state.appSources.sources);
-  const jobTitles = useSelector((state) => state.jobTitles.titles);
-  const environments = useSelector(
-    (state) => state.workEnvironments.environments
-  );
-
-  const [companyFilter, setCompanyFilter] = useState("");
-  const [sourceFilter, setSourceFilter] = useState("");
-  const [jobTitleFilter, setJobTitleFilter] = useState("");
-  const [environmentFilter, setEnvironmentFilter] = useState("");
+  // map the columns
+  const columns = [
+    { field: "id", headerName: "Id", width: 50 },
+    { field: "applicaitionDate", headerName: "Date", width: 200 },
+    { field: "source", headerName: "Source", width: 200 },
+    { field: "organization", headerName: "Organization", width: 200 },
+    { field: "jobTitle", headerName: "Job Title", width: 200 },
+    { field: "workEnvironment", headerName: "Office", width: 200 },
+  ];
 
   return (
     <div className="app">
       <Menu />
       <h1>Applications Submitted</h1>
       <div className="applications">
-        <h4>Filters</h4>
-        <div>
-          <label>Company Name:</label>
-          <input
-            type="text"
-            placeholder="Search Company Name..."
-            value={companyFilter}
-            onChange={(e) => setCompanyFilter(e.target.value)}
-            className="text-filter"
-          />
-          <label>Source:</label>
-          <select
-            value={sourceFilter}
-            onChange={(e) => setSourceFilter(e.target.value)}
-            className="select-filter"
-          >
-            <option value="">Select...</option>
-            {sources.map((option) => (
-              <option key={option.id} value={option.name}>
-                {option.name}
-              </option>
-            ))}
-          </select>
-          <label>Job Title:</label>
-          <select
-            value={jobTitleFilter}
-            onChange={(e) => setJobTitleFilter(e.target.value)}
-            className="select-filter"
-          >
-            <option>Select...</option>
-            {jobTitles.map((option) => (
-              <option key={option.id} value={option.name}>
-                {option.name}
-              </option>
-            ))}
-          </select>
-          <label>Location:</label>
-          <select
-            value={environmentFilter}
-            onChange={(e) => setEnvironmentFilter(e.target.value)}
-            className="select-filter"
-          >
-            <option>Select...</option>
-            {environments.map((option) => (
-              <option key={option.id} value={option.name}>
-                {option.name}
-              </option>
-            ))}
-          </select>
-        </div>
-        <div>
-          <h5>filter values:</h5>
-          <div>
-            company = {companyFilter} | source = {sourceFilter} | jobTitle ={" "}
-            {jobTitleFilter} | environment = {environmentFilter}
-          </div>
+        <div className="filter-bar">
+          <DataGrid rows={formattedAppData} columns={columns} pageSize={50} />
         </div>
       </div>
     </div>
