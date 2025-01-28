3ac31f37f930e726ca6b0a89dbe97e98ca75dd08
John Smolinski <john.smolinski@gmail.com>
2025-01-28 15:35:32 -0800

    adding mui form controls, cleaning up add application form. adding newApplicaton object

--- webui/app-tracker/src/components/Pages/AddApplication/AddApplication.js
+++ webui/app-tracker/src/components/Pages/AddApplication/AddApplication.js
@@ -1,6 +1,16 @@
 import React, { useState } from "react";
 import { useSelector } from "react-redux";
-import { Autocomplete, TextField } from "@mui/material";
+import dayjs from "dayjs";
+import {
+  Autocomplete,
+  TextField,
+  FormControl,
+  FormLabel,
+  Box,
+} from "@mui/material";
+import { LocalizationProvider } from "@mui/x-date-pickers/LocalizationProvider";
+import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
+import { DatePicker } from "@mui/x-date-pickers/DatePicker";
 import Menu from "../../Menu/Menu";
 import "./AddApplication.css";
 
@@ -15,6 +25,10 @@ export default function AddApplication() {
     (state) => state.workEnvironments.environments
   );
 
+  // application date
+  const [appDate, setAppDate] = useState(dayjs(new Date()));
+
+  // application properties
   const [selectedSource, setSelectedSource] = useState("");
   const [selectedOrganization, setSelectedOrganization] = useState("");
   const [selectedTitle, setSelectedTitle] = useState("");
@@ -45,139 +59,186 @@ export default function AddApplication() {
     return filtered;
   };
 
+  let newApplication = {
+    applicationDate: null,
+    source: { id: null, name: "" },
+    organization: { id: null, name: "" },
+    jobTitle: { id: null, name: "" },
+    workEnvironment: { id: null, name: "" },
+  };
+
   return (
     <div className="app">
       <Menu />
       <h2>Add Application</h2>
       {/* outter boundry */}
       <div className="add-application">
-        <div>
-          <label>Date:</label> Date Picker
-        </div>
-        {/* select/add source control */}
-        <div className="add-application">
-          Source:
-          <Autocomplete
-            sx={{ width: 300 }}
-            value={selectedSource}
-            onChange={handleChange(setSelectedSource)}
-            filterOptions={filteredOptions}
-            selectOnFocus
-            clearOnBlur
-            handleHomeEndKeys
-            options={sources.map((x) => x.name)}
-            getOptionLabel={(option) =>
-              typeof option === "string" ? option : option.label
-            }
-            renderOption={(props, option) => (
-              <li {...props}>
-                {typeof option === "string" ? option : option.label}
-              </li>
-            )}
-            freeSolo
-            renderInput={(params) => (
-              <TextField
-                {...params}
-                label="Select or Add new"
-                variant="outlined"
-                className="auto-complete"
+        {/* box enclosing form */}
+        <Box display="flex" flexDirection="column" gap={2}>
+          {/* Box for date picker*/}
+          <Box display="flex" alignItems="center">
+            <Box flex="0 0 150px">
+              <FormControl>
+                <FormLabel>Application Date:</FormLabel>
+              </FormControl>
+            </Box>
+            <Box flex="1">
+              <LocalizationProvider dateAdapter={AdapterDayjs}>
+                <DatePicker
+                  value={appDate}
+                  onChange={(newValue) => setAppDate(newValue)}
+                />
+              </LocalizationProvider>
+            </Box>
+          </Box>
+          {/* select/add source control */}
+          <Box display="flex" alignItems="center">
+            <Box flex="0 0 150px">
+              <FormControl>
+                <FormLabel>Source:</FormLabel>
+              </FormControl>
+            </Box>
+            <Box flex="1">
+              <Autocomplete
+                sx={{ width: 300 }}
+                value={selectedSource}
+                onChange={handleChange(setSelectedSource)}
+                filterOptions={filteredOptions}
+                selectOnFocus
+                clearOnBlur
+                handleHomeEndKeys
+                options={sources.map((x) => x.name)}
+                getOptionLabel={(option) =>
+                  typeof option === "string" ? option : option.label
+                }
+                renderOption={(props, option) => (
+                  <li {...props}>
+                    {typeof option === "string" ? option : option.label}
+                  </li>
+                )}
+                freeSolo
+                renderInput={(params) => (
+                  <TextField
+                    {...params}
+                    label="Select or Add new"
+                    variant="outlined"
+                    className="auto-complete"
+                  />
+                )}
               />
-            )}
-          />
-        </div>
-        {/* select/add organization control */}
-        <div className="add-application">
-          Organization:
-          <Autocomplete
-            sx={{ width: 300 }}
-            value={selectedOrganization}
-            onChange={handleChange(setSelectedOrganization)}
-            filterOptions={filteredOptions}
-            selectOnFocus
-            clearOnBlur
-            handleHomeEndKeys
-            options={organizations.map((x) => x.name)}
-            getOptionLabel={(option) =>
-              typeof option === "string" ? option : option.label
-            }
-            renderOption={(props, option) => (
-              <li {...props}>
-                {typeof option === "string" ? option : option.label}
-              </li>
-            )}
-            freeSolo
-            renderInput={(params) => (
-              <TextField
-                {...params}
-                label="Select or Add new"
-                variant="outlined"
-                className="auto-complete"
+            </Box>
+          </Box>
+          {/* select/add organization control */}
+          <Box display="flex" alignItems="center">
+            <Box flex="0 0 150px">
+              <FormControl>
+                <FormLabel>Organization:</FormLabel>
+              </FormControl>
+            </Box>
+            <Autocomplete
+              sx={{ width: 300 }}
+              value={selectedOrganization}
+              onChange={handleChange(setSelectedOrganization)}
+              filterOptions={filteredOptions}
+              selectOnFocus
+              clearOnBlur
+              handleHomeEndKeys
+              options={organizations.map((x) => x.name)}
+              getOptionLabel={(option) =>
+                typeof option === "string" ? option : option.label
+              }
+              renderOption={(props, option) => (
+                <li {...props}>
+                  {typeof option === "string" ? option : option.label}
+                </li>
+              )}
+              freeSolo
+              renderInput={(params) => (
+                <TextField
+                  {...params}
+                  label="Select or Add new"
+                  variant="outlined"
+                  className="auto-complete"
+                />
+              )}
+            />
+          </Box>
+
+          {/* select/add job title control */}
+          <Box display="flex" alignItems="center">
+            <Box flex="0 0 150px">
+              <FormControl>
+                <FormLabel>Job title:</FormLabel>
+              </FormControl>
+            </Box>
+            <Box flex="1">
+              <Autocomplete
+                sx={{ width: 300 }}
+                value={selectedTitle}
+                onChange={handleChange(setSelectedTitle)}
+                filterOptions={filteredOptions}
+                selectOnFocus
+                clearOnBlur
+                handleHomeEndKeys
+                options={jobTitles.map((x) => x.name)}
+                getOptionLabel={(option) =>
+                  typeof option === "string" ? option : option.label
+                }
+                renderOption={(props, option) => (
+                  <li {...props}>
+                    {typeof option === "string" ? option : option.label}
+                  </li>
+                )}
+                freeSolo
+                renderInput={(params) => (
+                  <TextField
+                    {...params}
+                    label="Select or Add new"
+                    variant="outlined"
+                    className="auto-complete"
+                  />
+                )}
               />
-            )}
-          />
-        </div>
-        {/* select/add job title control */}
-        <div className="add-application">
-          Job title:
-          <Autocomplete
-            sx={{ width: 300 }}
-            value={selectedTitle}
-            onChange={handleChange(setSelectedTitle)}
-            filterOptions={filteredOptions}
-            selectOnFocus
-            clearOnBlur
-            handleHomeEndKeys
-            options={jobTitles.map((x) => x.name)}
-            getOptionLabel={(option) =>
-              typeof option === "string" ? option : option.label
-            }
-            renderOption={(props, option) => (
-              <li {...props}>
-                {typeof option === "string" ? option : option.label}
-              </li>
-            )}
-            freeSolo
-            renderInput={(params) => (
-              <TextField
-                {...params}
-                label="Select or Add new"
-                variant="outlined"
-                className="auto-complete"
+            </Box>
+          </Box>
+          {/* select/add environment control */}
+          <Box display="flex" alignItems="center">
+            <Box flex="0 0 150px">
+              <FormControl>
+                <FormLabel>Environment:</FormLabel>
+              </FormControl>
+            </Box>
+            <Box flex="1">
+              <Autocomplete
+                sx={{ width: 300 }}
+                value={selectedEnvironment}
+                onChange={handleChange(setSelectedEnvironment)}
+                filterOptions={filteredOptions}
+                selectOnFocus
+                clearOnBlur
+                handleHomeEndKeys
+                options={environments.map((x) => x.name)}
+                getOptionLabel={(option) =>
+                  typeof option === "string" ? option : option.label
+                }
+                renderOption={(props, option) => (
+                  <li {...props}>
+                    {typeof option === "string" ? option : option.label}
+                  </li>
+                )}
+                freeSolo
+                renderInput={(params) => (
+                  <TextField
+                    {...params}
+                    label="Select or Add new"
+                    variant="outlined"
+                    className="auto-complete"
+                  />
+                )}
               />
-            )}
-          />
-        </div>
-        {/* insert work environment here */}
-        <div className="add-application">
-          Environment:
-          <Autocomplete
-            sx={{ width: 300 }}
-            value={selectedEnvironment}
-            onChange={handleChange(setSelectedEnvironment)}
-            filterOptions={filteredOptions}
-            selectOnFocus
-            clearOnBlur
-            handleHomeEndKeys
-            options={environments.map((x) => x.name)}
-            getOptionLabel={(option) =>
-              typeof option === "string" ? option : option.label
-            }
-            renderOption={(props, option) => (
-              <li {...props}>
-                {typeof option === "string" ? option : option.label}
-              </li>
-            )}
-            freeSolo
-            renderInput={(params) => (
-              <TextField
-                {...params}
-                label="Select or Add new"
-                variant="outlined"
-                className="auto-complete"
-              />
-            )}
-          />
-        </div>
+            </Box>
+          </Box>
+        </Box>
       </div>
     </div>
   );
